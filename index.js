b4fbc0aef0f775b6692545204a9e7142f5c2b05b
Artem <35739625+Artem26r@users.noreply.github.com>
2022-11-27 23:25:35 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+Highcharts.chart('container', {
+    chart: w.general,
+    title: w.title,
+    colors: w.colors,
+    tooltip: w.tooltip,
+    accessibility: w.accessibility,
+    legend: w.legend,
+    plotOptions: w.plotOptions,
+    series: w.series,
+    drilldown: w.drilldown,
+    credits: w.credits
+});
+
+
